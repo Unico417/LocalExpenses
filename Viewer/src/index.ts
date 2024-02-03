1d00af95accff24d0dcbcd4617362679a86ec79b
Unico417 <0417unico@gmail.com>
2024-02-03 11:15:45 +0900

    Change eol from CRLF to LF

--- Viewer/src/index.ts
+++ Viewer/src/index.ts
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Sample from "./sample";
 
-ReactDOM.createRoot(document.querySelector('#main')!)
-    .render(React.createElement(Sample, {}));
-console.log('Hello World!');
+
+function onDocumentLoad() {
+    // React entry sample
+    ReactDOM.createRoot(document.querySelector('#main')!)
+        .render(React.createElement(Sample, {}));
+}
+
+window.addEventListener('load', onDocumentLoad);
+
